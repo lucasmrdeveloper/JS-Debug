237a93be4e7218fbd491c159276dc6f58612f082
lucasmrdeveloper <lucasrodrigues.mr@gmail.com>
2026-03-30 15:31:30 -0300

    Chore: Remove comentários do PreviewConteudo

--- src/components/Colunas/ColunaDireita/PreviewConteudo.jsx
+++ src/components/Colunas/ColunaDireita/PreviewConteudo.jsx
@@ -1,19 +1,8 @@
 // Lógica
 import usePreview from "../../../hooks/usePreview";
 
+
 function PreviewConteudo( contexto ) {
-    console.log("\n\n\n")
- /*    console.log('%c ===== Preview - Conteudo ===== ', 'color: #00ff88; background: #1a1a2e; font-size: 14px; font-weight: bold; padding: 8px 16px; border-radius: 4px'); */
-
-    /* console.log("contexto", contexto)
-    console.log("contexto.titulo:", contexto.titulo)
-    console.log("contexto.descricao:", contexto.descricao)
-    console.log("contexto.textos:", contexto.textos)
-    console.log("contexto.box:", contexto.box)
-    
-    
-
-    console.log("preview do conteudo", preview) */
     const preview = usePreview(contexto)
 
     return(
