2d72b389e1d95c5b75252250f3aef2983e19efcc
lucasmrdeveloper <lucasrodrigues.mr@gmail.com>
2026-03-30 15:26:25 -0300

    Chore: Remove comentário no Preview

--- src/components/Colunas/ColunaDireita/Preview.jsx
+++ src/components/Colunas/ColunaDireita/Preview.jsx
@@ -10,13 +10,7 @@ import { useContext } from "react";
 
 
 function Preview() {
-    /*console.log("\n\n")
-     console.log('%c ===== 2 - Preview ===== ', 'color: #00ff88; background: #1a1a2e; font-size: 14px; font-weight: bold; padding: 8px 16px; border-radius: 4px'); */
-
     const { estado } = useContext(EstadoContext)
-    //const preview = usePreview(estado)
-        
-    //console.log("preview", preview)
 
     return(
        <div className="preview">
