8b7ae9a1c48cce19226dfd85e1f415c1bab02616
lucasmrdeveloper <lucasrodrigues.mr@gmail.com>
2026-04-02 16:50:37 -0300

    Chore: Remove dialogRef

--- src/hooks/validarInputModal.js
+++ src/hooks/validarInputModal.js
@@ -3,7 +3,7 @@ import salvarPreset from "./salvarPreset";
 function validarInputModal() {
     const { salvar } = salvarPreset()
 
-    function hundleSalvar(inputRef, dialogRef, setErro, fechar) {
+    function hundleSalvar(inputRef, setErro, fechar) {
         const nomePreset = inputRef.current.value.trim()
         const msgErro = "O campo está vazio, digite um nome para o preset."
     
